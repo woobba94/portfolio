9f961b760a423eb0a2c6817782549bb03bbdbe33
woobba94 <jwj3199@naver.com>
2022-02-04 21:47:47 +0900

    ✨Create : GreetingLine (temp)

---
+++ src/components/GreetingsLine.js
@@ -0,0 +1,14 @@
+function GreetingsLine() {
+  return (
+    <div className="GreetingsLine">
+      <svg viewBox="0 0 1150 377" fill="none">
+        <path
+          d="M0.5 178.5C401.268 240.367 875 224.5 1034 178.5C1193 132.5 1191.5 13 1002 2.00001C812.5 -8.99998 641.624 95.306 503 376"
+          stroke="black"
+          display="none"
+        />
+      </svg>
+    </div>
+  );
+}
+export default GreetingsLine;
