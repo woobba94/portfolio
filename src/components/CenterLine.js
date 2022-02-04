02b649c7c2d59dabaf0eac19a45a73cfb2dad1d3
woobba94 <jwj3199@naver.com>
2022-02-04 21:47:24 +0900

    ✨Create : CenterLine

---
+++ src/components/CenterLine.js
@@ -0,0 +1,10 @@
+function CenterLine() {
+  return (
+    <div className="CenterLine">
+      <svg viewBox="0 0 2 2868" fill="none" preserveAspectRatio="xMidYMax meet">
+        <path d="M1 1V2866.5" stroke="#AE1A1A" strokeWidth="2" strokeLinecap="round" />
+      </svg>
+    </div>
+  );
+}
+export default CenterLine;
