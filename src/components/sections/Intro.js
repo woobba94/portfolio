4311bab03a0b8cd0dd8e99ee8c2db16beb2b1646
woobba94 <jwj3199@naver.com>
2022-02-04 21:48:29 +0900

    ✨Create : Intro

---
+++ src/components/sections/Intro.js
@@ -0,0 +1,13 @@
+function Intro() {
+  return (
+    <div className="Intro">
+      <div className="left">
+        <p>방문해 주셔서 감사합니다.</p>
+        <p>컴퓨팅 사고를 즐기는 프론트엔드 개발자</p>
+        <p>정우진입니다.</p>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+}
+export default Intro;
