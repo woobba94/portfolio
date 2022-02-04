ad1ce3ca6373a71b417ceee8346ec6c75d70c2c3
woobba94 <jwj3199@naver.com>
2022-02-04 21:48:52 +0900

    ✨Create : Skills

---
+++ src/components/sections/Skills.js
@@ -0,0 +1,24 @@
+function Skills() {
+  return (
+    <div className="Skills">
+      <div className="icon-wrap">
+        <i className="icon-html fab fa-html5 fa-8x"></i>
+        <i className="icon-css fab fa-css3 fa-8x"></i>
+        <i className="icon-js fab fa-js fa-8x"></i>
+        <i className="icon-react fab fa-react fa-8x"></i>
+      </div>
+      <div className="text-wrap">
+        <div className="text-wrap-child">
+          <div className="left">
+            <div>HTML</div>
+          </div>
+          <p>시멘틱한 구조작성을 위해 노력합니다.</p>
+        </div>
+        <div className="text-wrap-child"></div>
+        <div className="text-wrap-child"></div>
+        <div className="text-wrap-child"></div>
+      </div>
+    </div>
+  );
+}
+export default Skills;
