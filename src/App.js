1724fddf969238e90abd4382a85b6217359cc65a
woobba94 <jwj3199@naver.com>
2022-02-04 21:51:48 +0900

    ✨Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { CenterLine, Header, Body, Footer } from './components/index';
+import GreetingsLine from './components/GreetingsLine';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CenterLine />
+      <GreetingsLine />
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+window.onload = function () {
+  // flag
+  let introFlag = false;
+  let skillFlag = false;
+
+  let path = document.querySelector('.CenterLine svg path');
+  let pathLength = path.getTotalLength();
+  console.log(pathLength);
+  path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+  path.style.strokeDashoffset = pathLength;
+
+  window.addEventListener('scroll', () => {
+    // 얼마나 스크롤된 상태인지 저장
+    var scrollPercentage =
+      (document.documentElement.scrollTop + document.body.scrollTop) /
+      (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    console.log(scrollPercentage);
+    // Length to offset the dashes
+    var drawLength = pathLength * scrollPercentage * 0.9;
+    if (!skillFlag && scrollPercentage > 0.41) {
+      skillFlag = true;
+      console.log('아이콘 펼치기');
+      let iconWrap = document.querySelector('.Skills .icon-wrap');
+      iconWrap.classList.add('unfold-icon');
+    } else if (!introFlag && scrollPercentage > 0.136) {
+      introFlag = true;
+      console.log('인사말 도형 드로우 시작');
+      let greetingsLinepath = document.querySelector('.GreetingsLine svg path');
+      greetingsLinepath.style.display = 'block';
+      greetingsLinepath.classList.add('draw');
+    }
+    // Draw in reverse
+    path.style.strokeDashoffset = pathLength - drawLength;
+  });
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +46,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
