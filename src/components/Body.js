463ed73fe345d29b36e538e12cd7244e6ad545b6
woobba94 <jwj3199@naver.com>
2022-02-04 21:47:13 +0900

    ✨Create : Body

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import Section from './Section';
+
+function Body() {
+  return (
+    <div className="Body">
+      <Section name="Intro" />
+      <Section name="Greetings" />
+      <Section name="Skills" />
+      <Section name="Projects" />
+      <Section name="Contact" />
+    </div>
+  );
+}
+
+export default Body;
