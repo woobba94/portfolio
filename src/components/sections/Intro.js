c10d253e9f9d5923cedc820140bd7812013d92a9
woobba94 <jwj3199@naver.com>
2022-02-07 02:19:36 +0900

    ✅ Update : Intro effect

--- src/components/sections/Intro.js
+++ src/components/sections/Intro.js
@@ -1,7 +1,16 @@
+import myVideo from './Intro-bg.mp4';
 function Intro() {
   return (
     <div className="Intro">
-      <div className="left">
+      {/* <video src={myVideo} muted autoPlay loop></video> */}
+      {/* <div className="hourglass"></div> */}
+      <canvas id="forest"></canvas>
+      <div className="left scroll-down" data-rate=".3" data-direction="vertical">
+        <p>방문해 주셔서 감사합니다.</p>
+        <p>컴퓨팅 사고를 즐기는 프론트엔드 개발자</p>
+        <p>정우진입니다.</p>
+      </div>
+      <div className="shadow scroll-down" data-rate=".5" data-direction="vertical">
         <p>방문해 주셔서 감사합니다.</p>
         <p>컴퓨팅 사고를 즐기는 프론트엔드 개발자</p>
         <p>정우진입니다.</p>
