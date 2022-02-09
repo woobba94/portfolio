52e2871097297faf0585dbe2f352837e0c34cf80
woobba94 <jwj3199@naver.com>
2022-02-09 14:38:57 +0900

    🔥 Remove : test code

--- src/components/sections/Intro.js
+++ src/components/sections/Intro.js
@@ -8,7 +8,6 @@ function Intro() {
       {/* <div className="hourglass"></div> */}
       <div className="left scroll-down" data-rate=".3" data-direction="vertical">
         <p>방문해 주셔서 감사합니다.</p>
-        <p>배포 테스트</p>
         <p>컴퓨팅 사고를 즐기는 프론트엔드 개발자</p>
         <p>정우진입니다.</p>
       </div>
