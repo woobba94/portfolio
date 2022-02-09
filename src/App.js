d46542ce6ec84ddb804133dd669380e752cec0b2
woobba94 <jwj3199@naver.com>
2022-02-09 14:05:20 +0900

    ✏️ FIx typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { CenterLine, Header, Body, Footer } from './components/index';
 import GreetingsLine from './components/GreetingsLine';
+// import { useState } from 'react';
+// import Modal from './components/Modal';
 function App() {
   return (
     <div className="App">
