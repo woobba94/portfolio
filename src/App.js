b946e0124664e41f99923c2df2f5f1a2e8b1ee80
woobba94 <jwj3199@naver.com>
2022-02-19 23:53:58 +0900

    ♻️ Refactor : ScrollEvent logic

--- src/App.js
+++ src/App.js
@@ -2,44 +2,48 @@ import './App.css';
 import { Header, Body, Footer } from './components/index';
 import GreetingsLine from './components/GreetingsLine';
 import CenterLine from './components/CenterLine';
-import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // import Modal from './components/Modal';
 function App() {
+  console.log('App 렌더링');
   const centerLine = useRef();
   // 얼마나 스크롤된 상태인지 저장
   const [scrollPercentage, setScrollPercentage] = useState(0);
-  const [drawLength, setDrawLength] = useState(0);
-  const [pathLength, setPathLength] = useState(0);
+  let drawLength = useRef(0);
+  let pathLength = useRef(0);
 
-  const [greetingFlag, setGreetingFlag] = useState(false);
-  const [skillFlag, setSkillFlag] = useState(false);
+  // flag
+  let greetingFlag = useRef(false);
+  let skillsFlag = useRef(false);
+
   // 그려지는 속도 가중치 1.0 = 1배
   const speed = 1.2;
   let path;
-  // const pathLength = path.getTotalLength();
-  // console.log(`drawLength ${drawLength}`);
+  drawLength.current =
+    ((pathLength.current * window.pageYOffset) /
+      (document.documentElement.scrollHeight - document.documentElement.clientHeight)) *
+    speed;
 
   useEffect(() => {
     path = centerLine.current;
-    setPathLength(path.getTotalLength());
-    path.style.strokeDasharray = pathLength + ' ' + pathLength;
-    path.style.strokeDashoffset = pathLength;
-    const onScroll = () => {
+    pathLength.current = path.getTotalLength();
+    path.style.strokeDasharray = pathLength.current + ' ' + pathLength.current;
+    path.style.strokeDashoffset = pathLength.current;
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
       setScrollPercentage(
         window.pageYOffset /
           (document.documentElement.scrollHeight - document.documentElement.clientHeight)
       );
-      setDrawLength(
-        ((pathLength * window.pageYOffset) /
-          (document.documentElement.scrollHeight - document.documentElement.clientHeight)) *
-          speed
-      );
     };
-    window.removeEventListener('scroll', onScroll);
-    window.addEventListener('scroll', onScroll, { passive: true });
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll); //clean up
+    };
+  }, [scrollPercentage]);
 
-    return () => window.removeEventListener('scroll', onScroll);
-  }, [pathLength]);
   // console.log(offset);
   function setProgress() {
     console.log('setProgress()');
@@ -73,53 +77,25 @@ function App() {
       progressTexts.style = '';
     });
   }
-
-  if (!skillFlag && scrollPercentage > 0.31) {
-    setSkillFlag(true);
+  if (!skillsFlag.current && scrollPercentage > 0.31) {
     console.log('아이콘 펼치기');
+    skillsFlag.current = true;
+    setTimeout(setProgress, 2500);
     let iconWrap = document.querySelector('.Skills .skills-wrap');
     iconWrap.classList.add('unfold-icon');
     let skillsAnimationBox = document.querySelector('.Skills').firstChild;
     skillsAnimationBox.classList.add('animation-box');
-    setTimeout(setProgress, 2500);
-  } else if (!greetingFlag && scrollPercentage > 0.13) {
-    setGreetingFlag(true);
-    console.log('Greeting right 페이드 시작');
-    document.querySelector('.Greetings .right').classList.add('fade-right');
-    // let greetingsLinepath = document.querySelector('.GreetingsLine svg path');
-    // greetingsLinepath.style.display = 'block';
-    // greetingsLinepath.classList.add('draw');
   }
 
-  // scroll-down object
-  const downTarget = document.querySelectorAll('.scroll-down');
-  for (let i = 0; i < downTarget.length; i++) {
-    let pos = window.pageYOffset * downTarget[i].dataset.rate;
-    if (downTarget[i].dataset.direction === 'vertical') {
-      downTarget[i].style.transform = 'translate3d(0px, ' + pos + 'px, 0px)';
-    } else {
-      let posX = window.pageYOffset * downTarget[i].dataset.ratex;
-      let posY = window.pageYOffset * downTarget[i].dataset.ratey;
-      downTarget[i].style.transform = 'translate3d(' + posX + 'px,' + posY + 'px,0px)';
-    }
+  if (!greetingFlag.current && scrollPercentage > 0.13) {
+    console.log('Greeting right 페이드 시작');
+    greetingFlag.current = true;
+    document.querySelector('.Greetings .right').classList.add('fade-right');
   }
-  // scroll-up object
-  const upTarget = document.querySelectorAll('.scroll-up');
-  for (let i = 0; i < upTarget.length; i++) {
-    let pos = window.pageYOffset * upTarget[i].dataset.rate;
-    if (upTarget[i].dataset.direction === 'vertical') {
-      upTarget[i].style.transform = 'translate3d(0px, ' + -pos + 'px, 0px)';
-    } else {
-      let posX = window.pageYOffset * upTarget[i].dataset.ratex;
-      let posY = window.pageYOffset * upTarget[i].dataset.ratey;
-      upTarget[i].style.transform = 'translate3d(' + -posX + 'px,' + -posY + 'px,0px)';
-    }
-  }
-  // console.log(`pathLength : ${pathLength}`);
-  // console.log(`drawLength : ${drawLength}`);
+
   return (
     <div className="App">
-      <CenterLine ref={centerLine} strokeDashoffset={pathLength - drawLength} />
+      <CenterLine ref={centerLine} strokeDashoffset={pathLength.current - drawLength.current} />
       <GreetingsLine />
       <Header />
       <Body />
