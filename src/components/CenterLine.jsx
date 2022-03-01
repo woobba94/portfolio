f4a6c9c0696035cf1b92c53da8a319acfb10d5eb
woobba94 <jwj3199@naver.com>
2022-03-01 23:22:05 +0900

    ✅ Update : CenterLine style

--- src/components/CenterLine.jsx
+++ src/components/CenterLine.jsx
@@ -1,5 +1,21 @@
 import React, { useContext, useRef, useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { ScrollContext } from '../context/ScrollContext';
+
+const Wrapper = styled.div`
+  height: 500vh;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  overflow: hidden;
+  z-index: 1;
+  svg {
+    height: 100%;
+  }
+`;
+
 const CenterLine = (_props) => {
   const scrollPercentage = useContext(ScrollContext);
 
@@ -22,17 +38,7 @@ const CenterLine = (_props) => {
   }, [scrollPercentage]);
 
   return (
-    // <div className="CenterLine" onScroll={callbackFunc}>
-    <div className="CenterLine">
-      {/* <svg viewBox="0 0 45 2854" fill="none" preserveAspectRatio="xMidYMax meet">
-        <path
-          style={{ strokeDashoffset: pathLength.current - drawLength }}
-          ref={path}
-          d="M21.9999 0.5V292.5C23.5412 296.224 23.2521 298.009 21.9999 301C17.7066 307.808 18.2079 310.583 21.9999 314.5C46.8595 332.158 50.589 342.504 21.9999 362.5C-4.86933 381.677 -2.60575 391.647 21.9999 408.5C48.8153 423.117 47.0755 432.062 21.9999 449C14.9317 454.729 15.2894 458.617 21.9999 466.5C23.5858 469.417 23.9345 471.389 21.9999 476.5V2853.5"
-          stroke="#f44957"
-          strokeWidth="3"
-        />
-      </svg> */}
+    <Wrapper>
       <svg
         viewBox="0 0 207 2854"
         fill="none"
@@ -47,7 +53,7 @@ const CenterLine = (_props) => {
           strokeLinecap="round"
         />
       </svg>
-    </div>
+    </Wrapper>
   );
 };
 
