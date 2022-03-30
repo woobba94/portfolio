3171077b8d7bdc929d7dd013d12b119802b0acc2
woobba94 <jwj3199@naver.com>
2022-03-30 21:52:30 +0900

    ♻️ Refactor: Experience component
    - 사용되지 않는 state 삭제
    - 중복된 코드들을 map을 이용하여 제거

--- src/components/Section/Experience/Experience.jsx
+++ src/components/Section/Experience/Experience.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { ScrollContext } from '../../../context/ScrollContext';
 import * as Styled from './styled';
 import data from './data';
@@ -9,54 +9,42 @@ function Experience() {
   let flag = useRef(false);
   const buttonWrapRef = useRef();
   const itemWrapRef = useRef();
-  const [target, setTarget] = useState();
+
   useEffect(() => {
     if (!flag.current && scrollPercentage > 0.529) {
       flag.current = true;
       buttonWrapRef.current.firstChild.click();
     }
   }, [scrollPercentage]);
+
   function targetHandler(index) {
     for (let i = 0; i < buttonWrapRef.current.children.length; i++) {
       if (index === i) {
-        buttonWrapRef.current.children[i].firstChild.style.background =
-          'var(--color-point-1)';
+        buttonWrapRef.current.children[i].firstChild.style.background = 'var(--color-point-1)';
         itemWrapRef.current.children[i].style.opacity = '1';
       } else {
-        buttonWrapRef.current.children[i].firstChild.style.background =
-          'var(--color-main-2)';
+        buttonWrapRef.current.children[i].firstChild.style.background = 'var(--color-main-2)';
         itemWrapRef.current.children[i].style.opacity = '0';
       }
     }
-    setTarget(index);
   }
+
   return (
     <Styled.Wrapper id="Experience">
       <Styled.ButtonWrap ref={buttonWrapRef}>
-        <Styled.ButtonBox onClick={() => targetHandler(0)}>
-          <Styled.Button />
-        </Styled.ButtonBox>
-        <Styled.ButtonBox onClick={() => targetHandler(1)}>
-          <Styled.Button />
-        </Styled.ButtonBox>
-        <Styled.ButtonBox onClick={() => targetHandler(2)}>
-          <Styled.Button />
-        </Styled.ButtonBox>
-        <Styled.ButtonBox onClick={() => targetHandler(3)}>
-          <Styled.Button />
-        </Styled.ButtonBox>
-        <Styled.ButtonBox onClick={() => targetHandler(4)}>
-          <Styled.Button />
-        </Styled.ButtonBox>
+        {[0, 1, 2, 3, 4].map((index) => {
+          return (
+            <Styled.ButtonBox onClick={() => targetHandler(index)}>
+              <Styled.Button />
+            </Styled.ButtonBox>
+          );
+        })}
       </Styled.ButtonWrap>
       <div ref={itemWrapRef}>
-        <Item {...data[0]} />
-        <Item {...data[1]} />
-        <Item {...data[2]} />
-        <Item {...data[3]} />
-        <Item {...data[4]} />
+        {[0, 1, 2, 3, 4].map((index) => {
+          return <Item {...data[index]} />;
+        })}
       </div>
-      {/* {target !== undefined && <Item {...data[target]} style={ItemWrapper} />} */}
     </Styled.Wrapper>
   );
 }
